9afb958ff5ea60fdbb35692e4400a32a71f40cee
“Johnny <jaaragon@pucesi.edu.ec>
2025-06-26 20:55:38 -0500

    Actualziacon 3

--- app/Controllers/Suma.php
+++ app/Controllers/Suma.php
@@ -10,16 +10,25 @@ class Suma extends BaseController
     public function index()
     {
         $data = [
-            "titulo"=> "Vista SUMA",
+            "titulo" => "Vista SUMA",
         ];
         echo view("Contenido/Encabezado", $data);
         echo view("VistaFormulario/Cuerpo");
     }
 
-    public function psumar(){
-       $Vnumero1 = $this->request->getPost('numero1'); 
-       $Vnumero2 = $this->request->getPost('numero2'); 
+    public function psumar()
+    {
+        $Vnumero1 = $this->request->getPost('numero1');
+        $Vnumero2 = $this->request->getPost('numero2');
+        $VResultado = $Vnumero1 + $Vnumero2;
 
-       echo($Vnumero1+ $Vnumero2);
+        $data = [
+            "numero1" => $Vnumero1,
+            "numero2" =>  $Vnumero2,
+            "resultado" => $VResultado,
+        ];
+
+
+        echo view('VistaFormulario/suma_resultado', $data);
     }
 }
---
+++ app/Views/VistaFormulario/suma_resultado.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Resultado</h2>
+    <p><?=$numero1?> + <?=$numero2?> = <?=$resultado?> </p>
+    <a href=" <?= base_url('/Suma') ?> ">Redireccionar a la Pagina Suma</a>
+</body>
+</html>
