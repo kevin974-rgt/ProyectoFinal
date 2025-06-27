3c9451a56995b22e4ba1f53eb384271c83d34f52
“Johnny <jaaragon@pucesi.edu.ec>
2025-06-26 20:36:24 -0500

    2

--- app/Views/Contenido/Encabezado.php
+++ app/Views/Contenido/Encabezado.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Vista Sumar</title>
+    <title><?= $titulo ?></title>
     <link rel="stylesheet" href="<?= base_url('public/css/bootstrap.min.css') ?>">
-    <script src="<? base_url('public/js/bootstrap.bundle.min.js') ?>"></script>
+    <script src="<?= base_url('public/js/bootstrap.bundle.min.js') ?>"></script>
 </head>
 <body>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
